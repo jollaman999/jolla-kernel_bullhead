0914eea2b2a4934d42ff2ae11846d66f7e696ada
Brandon Berhent <bbedward@androiddeveloperalliance.org>
2012-11-08 09:52:40 -0500

    V(R) update

--- block/vr-iosched.c
+++ block/vr-iosched.c
@@ -305,13 +305,6 @@ vr_move_request(vd, rq);
 return 1;
 }
 
-static int
-vr_queue_empty(struct request_queue *q)
-{
-struct vr_data *vd = vr_get_data(q);
-return RB_EMPTY_ROOT(&vd->sort_list);
-}
-
 static void
 vr_exit_queue(struct elevator_queue *e)
 {
@@ -414,7 +407,6 @@ static struct elevator_type iosched_vr = {
 .elevator_merge_req_fn = vr_merged_requests,
 .elevator_dispatch_fn = vr_dispatch_requests,
 .elevator_add_req_fn = vr_add_request,
-.elevator_queue_empty_fn = vr_queue_empty,
 .elevator_former_req_fn = elv_rb_former_request,
 .elevator_latter_req_fn = elv_rb_latter_request,
 .elevator_init_fn = vr_init_queue,
