2ff2d04830ddc0fbc6a621de6fdf9581e6385f33
Riley Andrews <riandrews@google.com>
2014-09-26 17:36:36 -0700

    staging: binder: Change binder mutex to rtmutex.
    Surfaceflinger uses binder heavily to receive/send frames from applications
    while compositing the screen. Change the binder mutex to an rt mutex to minimize
    instances where high priority surfaceflinger binder work is blocked by lower
    priority binder ipc.
    
    Signed-off-by: Riley Andrews <riandrews@google.com>
    Change-Id: I086a715267648448f0c5f62b037a3093d1079a79
    Git-commit: 5d03bd0fdd66d3472386864cf32b4b82dda8d1a5
    Git-repo: https://android.googlesource.com/kernel/common.git
    Signed-off-by: Ian Maund <imaund@codeaurora.org>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -26,6 +26,7 @@
 #include <linux/miscdevice.h>
 #include <linux/mm.h>
 #include <linux/module.h>
+#include <linux/rtmutex.h>
 #include <linux/mutex.h>
 #include <linux/nsproxy.h>
 #include <linux/poll.h>
@@ -42,7 +43,7 @@
 #include "binder.h"
 #include "binder_trace.h"
 
-static DEFINE_MUTEX(binder_main_lock);
+static DEFINE_RT_MUTEX(binder_main_lock);
 static DEFINE_MUTEX(binder_deferred_lock);
 static DEFINE_MUTEX(binder_mmap_lock);
 
@@ -420,7 +421,7 @@ static long task_close_fd(struct binder_proc *proc, unsigned int fd)
 static inline void binder_lock(const char *tag)
 {
 	trace_binder_lock(tag);
-	mutex_lock(&binder_main_lock);
+	rt_mutex_lock(&binder_main_lock);
 	preempt_disable();
 	trace_binder_locked(tag);
 }
@@ -428,7 +429,7 @@ static inline void binder_lock(const char *tag)
 static inline void binder_unlock(const char *tag)
 {
 	trace_binder_unlock(tag);
-	mutex_unlock(&binder_main_lock);
+	rt_mutex_unlock(&binder_main_lock);
 	preempt_enable();
 }
 
@@ -3173,7 +3174,7 @@ static void binder_deferred_func(struct work_struct *work)
 	int defer;
 	do {
 		trace_binder_lock(__func__);
-		mutex_lock(&binder_main_lock);
+		rt_mutex_lock(&binder_main_lock);
 		trace_binder_locked(__func__);
 
 		mutex_lock(&binder_deferred_lock);
@@ -3204,7 +3205,7 @@ static void binder_deferred_func(struct work_struct *work)
 			binder_deferred_release(proc); /* frees proc */
 
 		trace_binder_unlock(__func__);
-		mutex_unlock(&binder_main_lock);
+		rt_mutex_lock(&binder_main_lock);
 		preempt_enable_no_resched();
 		if (files)
 			put_files_struct(files);
