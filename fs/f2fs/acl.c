626e42c37655f4e9cd7543f5d062a3e4f0790492
Gu Zheng <guz.fnst@cn.fujitsu.com>
2014-10-20 17:45:52 +0800

    f2fs: remove the redundant function cond_clear_inode_flag
    Use clear_inode_flag to replace the redundant cond_clear_inode_flag.
    
    Signed-off-by: Gu Zheng <guz.fnst@cn.fujitsu.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/acl.c
+++ fs/f2fs/acl.c
@@ -246,7 +246,7 @@ static int f2fs_set_acl(struct inode *inode, int type,
 	if (acl) {
 		value = f2fs_acl_to_disk(acl, &size);
 		if (IS_ERR(value)) {
-			cond_clear_inode_flag(fi, FI_ACL_MODE);
+			clear_inode_flag(fi, FI_ACL_MODE);
 			return (int)PTR_ERR(value);
 		}
 	}
@@ -257,7 +257,7 @@ static int f2fs_set_acl(struct inode *inode, int type,
 	if (!error)
 		set_cached_acl(inode, type, acl);
 
-	cond_clear_inode_flag(fi, FI_ACL_MODE);
+	clear_inode_flag(fi, FI_ACL_MODE);
 	return error;
 }
 
