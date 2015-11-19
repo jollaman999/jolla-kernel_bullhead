df505145f30a53f31247a54f1c44a089ba0697ff
Amarnath Hullur Subramanyam <amarnath@codeaurora.org>
2015-11-18 20:20:37 -0800

    qcacld-2.0: Pass TDLS teardown notification interval to target
    Firmware uses same notification interval to send trigger to the
    driver for both TDLS setup and TDLS teardown operations.In ideal
    scenario firmware needs to have an option to configure different
    values for TDLS setup and TDLS tear down operations.This change
    will enable firmware to have different values for setup and
    teardown operations.
    
    Change-Id: I300350c56d4ef23993953338529f29df2ea122a1
    CRs-Fixed: 920835
    Bug: 25777726
    Signed-off-by: Amarnath Hullur Subramanyam <amarnath@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
+++ drivers/staging/qcacld-2.0/CORE/SERVICES/WMA/wma.c
@@ -28966,6 +28966,8 @@ static int wma_update_fw_tdls_state(WMA_HANDLE handle, void *pwmaTdlsparams)
 	    wma_tdls->puapsd_inactivity_time;
 	cmd->tdls_puapsd_rx_frame_threshold =
 	    wma_tdls->puapsd_rx_frame_threshold;
+	cmd->teardown_notification_ms =
+	    wma_tdls->teardown_notification_ms;
 
 	WMA_LOGD("%s: tdls_mode: %d, state: %d, "
 	         "notification_interval_ms: %d, "
@@ -28978,7 +28980,8 @@ static int wma_update_fw_tdls_state(WMA_HANDLE handle, void *pwmaTdlsparams)
 	         "tdls_peer_traffic_response_timeout: %d, "
 	         "tdls_puapsd_mask: 0x%x, "
 	         "tdls_puapsd_inactivity_time: %d, "
-	         "tdls_puapsd_rx_frame_threshold: %d ",
+	         "tdls_puapsd_rx_frame_threshold: %d "
+	         "teardown_notification_ms: %d ",
 	         __func__, tdls_mode, cmd->state,
 	         cmd->notification_interval_ms,
 	         cmd->tx_discovery_threshold,
@@ -28990,7 +28993,8 @@ static int wma_update_fw_tdls_state(WMA_HANDLE handle, void *pwmaTdlsparams)
 	         cmd->tdls_peer_traffic_response_timeout_ms,
 	         cmd->tdls_puapsd_mask,
 	         cmd->tdls_puapsd_inactivity_time_ms,
-	         cmd->tdls_puapsd_rx_frame_threshold);
+	         cmd->tdls_puapsd_rx_frame_threshold,
+	         cmd->teardown_notification_ms);
 
 	if (wmi_unified_cmd_send(wma_handle->wmi_handle, wmi_buf, len,
 		   WMI_TDLS_SET_STATE_CMDID)) {
