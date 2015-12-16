a480d6b77438a6fccd5d22597ed975fd2d175f45
Todd Kjos <tkjos@google.com>
2015-12-15 16:36:22 -0800

    Add support for making sched-dvfs the default cpufreq governor
    Change-Id: Ib0ea99b1778e6fcd3a7e90c5a70886654b66bb8a

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -457,6 +457,9 @@ extern struct cpufreq_governor cpufreq_gov_bioshock;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
 extern struct cpufreq_governor cpufreq_gov_blu_active;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHED)
+extern struct cpufreq_governor cpufreq_gov_sched;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_sched)
 #endif
 
 /*********************************************************************
