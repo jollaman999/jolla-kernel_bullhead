8c99e96236276ab26332bc9e3bea02c8ec472b48
jollaman999 <admin@jollaman999.com>
2016-01-20 21:33:13 +0900

    audit: Fix GCC 6 tautological-compare warning

--- kernel/audit.c
+++ kernel/audit.c
@@ -573,9 +573,18 @@ static int audit_netlink_ok(struct sk_buff *skb, u16 msg_type)
 {
 	int err = 0;
 
+	/*
+	 *  If 'CONFIG_USER_NS' is not defined, current_user_ns()
+	 * is already defined as '&init_user_ns'.
+	 *  In GCC 6 it makes tautological-compare warning.
+	 *  So just compare when 'CONFIG_USER_NS' is defined.
+	 * - jollaman999 -
+	 */
+#ifdef CONFIG_USER_NS
 	/* Only support initial user namespace for now. */
 	if ((current_user_ns() != &init_user_ns))
 		return -EPERM;
+#endif
 
 	switch (msg_type) {
 	case AUDIT_LIST:
