df30cb5172c32d5ee4518ece79ff8556ab71ae41
jollaman999 <admin@jollaman999.com>
2016-01-23 11:02:11 +0900

    msm_hotplug, fingerprint: Do not hotplugging when disabled

--- drivers/input/misc/fpc1020_tee.c
+++ drivers/input/misc/fpc1020_tee.c
@@ -48,6 +48,11 @@
 #include <linux/of_gpio.h>
 #endif
 #include <linux/wakelock.h>
+
+#ifdef CONFIG_MSM_HOTPLUG
+#include <linux/msm_hotplug.h>
+#endif
+
 #define FPC1020_RESET_LOW_US 1000
 #define FPC1020_RESET_HIGH1_US 100
 #define FPC1020_RESET_HIGH2_US 1250
@@ -734,7 +739,8 @@ static irqreturn_t fpc1020_irq_handler(int irq, void *handle)
 	if (fpc1020->wakeup_enabled ) {
 		wake_lock_timeout(&fpc1020->ttw_wl, msecs_to_jiffies(FPC_TTW_HOLD_TIME));
 #ifdef CONFIG_MSM_HOTPLUG
-		msm_hotplug_resume_timeout();
+		if (msm_enabled)
+			msm_hotplug_resume_timeout();
 #endif
 	}
 
