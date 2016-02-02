e9ac574956a960fbc6d2195395f20109e7e67930
jollaman999 <admin@jollaman999.com>
2016-02-02 14:54:21 +0900

    cpufreq: Fallback to interactive governor

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1858,8 +1858,8 @@ static int __cpufreq_governor(struct cpufreq_policy *policy,
 	   restrictions, like e.g. conservative or ondemand.
 	   That this is the case is already ensured in Kconfig
 	*/
-#ifdef CONFIG_CPU_FREQ_GOV_PERFORMANCE
-	struct cpufreq_governor *gov = &cpufreq_gov_performance;
+#ifdef CONFIG_CPU_FREQ_GOV_INTERACTIVE
+	struct cpufreq_governor *gov = &cpufreq_gov_interactive;
 #else
 	struct cpufreq_governor *gov = NULL;
 #endif
