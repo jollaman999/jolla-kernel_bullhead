5d810069f010f73d55e3488b8225a2cb1b970217
jollaman999 <admin@jollaman999.com>
2016-02-13 23:40:43 +0900

    kexec: Remove debug code

--- arch/arm64/kernel/machine_kexec.c
+++ arch/arm64/kernel/machine_kexec.c
@@ -171,7 +171,6 @@ void machine_kexec_cleanup(struct kimage *image)
  */
 int machine_kexec_prepare(struct kimage *image)
 {
-	unsigned long *hardboot_page;
 	kexec_image_info(image);
 	fill_bypass(image);
 	if (bypass_purgatory) {
@@ -184,10 +183,6 @@ int machine_kexec_prepare(struct kimage *image)
 #ifdef CONFIG_KEXEC_HARDBOOT
 	arm64_kexec_hardboot = image->hardboot;
 #endif
-	// debug; please remove
-	hardboot_page = ioremap(KEXEC_HB_PAGE_ADDR, SZ_1M);
-	pr_info("Last hardboot status: %lx\n", hardboot_page[0]);
-	iounmap(hardboot_page);
 
 	return 0;
 }
