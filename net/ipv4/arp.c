027db8d88d7d388c20b0e2f393171879418ea23c
Zhang Shengju <zhangshengju@cmss.chinamobile.com>
2016-03-04 14:07:54 +0000

    arp: correct return value of arp_rcv
    Currently, arp_rcv() always return zero on a packet delivery upcall.
    
    To make its behavior more compliant with the way this API should be
    used, this patch changes this to let it return NET_RX_SUCCESS when the
    packet is proper handled, and NET_RX_DROP otherwise.
    
    v1->v2:
    If sanity check is failed, call kfree_skb() instead of consume_skb(), then
    return the correct return value.
    
    Signed-off-by: Zhang Shengju <zhangshengju@cmss.chinamobile.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>

--- net/ipv4/arp.c
+++ net/ipv4/arp.c
@@ -880,7 +880,7 @@ static int arp_process(struct sk_buff *skb)
 	 */
 
 	if (in_dev == NULL)
-		goto out;
+		goto out_free_skb;
 
 	arp = arp_hdr(skb); // Get ARP header from sk buff
 
@@ -895,7 +895,7 @@ static int arp_process(struct sk_buff *skb)
 	default:
 		if (arp->ar_pro != htons(ETH_P_IP) ||
 		    htons(dev_type) != arp->ar_hrd)
-			goto out;
+			goto out_free_skb;
 		break;
 	case ARPHRD_ETHER:	// Ethernet is here
 	case ARPHRD_FDDI:
@@ -912,17 +912,17 @@ static int arp_process(struct sk_buff *skb)
 		if ((arp->ar_hrd != htons(ARPHRD_ETHER) &&
 		     arp->ar_hrd != htons(ARPHRD_IEEE802)) ||
 		    arp->ar_pro != htons(ETH_P_IP))
-			goto out;
+			goto out_free_skb;
 		break;
 	case ARPHRD_AX25:
 		if (arp->ar_pro != htons(AX25_P_IP) ||
 		    arp->ar_hrd != htons(ARPHRD_AX25))
-			goto out;
+			goto out_free_skb;
 		break;
 	case ARPHRD_NETROM:
 		if (arp->ar_pro != htons(AX25_P_IP) ||
 		    arp->ar_hrd != htons(ARPHRD_NETROM))
-			goto out;
+			goto out_free_skb;
 		break;
 	}
 
@@ -930,7 +930,7 @@ static int arp_process(struct sk_buff *skb)
 
 	if (arp->ar_op != htons(ARPOP_REPLY) &&
 	    arp->ar_op != htons(ARPOP_REQUEST))
-		goto out;
+		goto out_free_skb;
 
 	/* arp_project - Print arp_ptr infos */
 	if (arp_project_enable && print_arp_info)
@@ -974,7 +974,7 @@ static int arp_process(struct sk_buff *skb)
 	*/
 	if (ipv4_is_multicast(tip) ||
 	    (!IN_DEV_ROUTE_LOCALNET(in_dev) && ipv4_is_loopback(tip)))
-		goto out;
+		goto out_free_skb;
 
  /*
   *	For some 802.11 wireless deployments (and possibly other networks),
@@ -982,7 +982,7 @@ static int arp_process(struct sk_buff *skb)
   *	and thus should not be accepted.
   */
 	if (sip == tip && IN_DEV_ORCONF(in_dev, DROP_GRATUITOUS_ARP))
-		goto out;
+		goto out_free_skb;
 
 /*
  *     Special case: We must set Frame Relay source Q.922 address
@@ -1024,11 +1024,10 @@ static int arp_process(struct sk_buff *skb)
 	if (sip == 0) {
 		if (arp->ar_op == htons(ARPOP_REQUEST) &&
 		    inet_addr_type(net, tip) == RTN_LOCAL &&
-
 		    !arp_ignore(in_dev, sip, tip))
 			arp_send(ARPOP_REPLY, ETH_P_ARP, sip, dev, tip, sha,
 				 dev->dev_addr, sha);
-		goto out;
+		goto out_consume_skb;
 	}
 
 
@@ -1059,7 +1058,7 @@ static int arp_process(struct sk_buff *skb)
 					neigh_release(n);
 				}
 			}
-			goto out; // End
+			goto out_consume_skb; // End
 		} else if (IN_DEV_FORWARD(in_dev)) { // Is IPv4 forwarding enabled?
 			// Proxy ARP
 			if (addr_type == RTN_UNICAST  &&
@@ -1084,7 +1083,7 @@ static int arp_process(struct sk_buff *skb)
 						       in_dev->arp_parms, skb);
 					return 0;
 				}
-				goto out; // End
+				goto out_consume_skb; // End
 			}
 		}
 	}
@@ -1179,9 +1178,13 @@ static inline struct neighbour *neigh_create(struct neigh_table *tbl,
 		neigh_release(n);
 	} // End
 
-out:
+out_consume_skb:
 	consume_skb(skb); // free an skbuff
-	return 0;
+	return NET_RX_SUCCESS;
+
+out_free_skb:
+	kfree_skb(skb);
+	return NET_RX_DROP;
 }
 
 static void parp_redo(struct sk_buff *skb)
@@ -1223,11 +1226,11 @@ static int arp_rcv(struct sk_buff *skb, struct net_device *dev,
 
 consumeskb:
 	consume_skb(skb);
-	return 0;
+	return NET_RX_SUCCESS;
 freeskb:
 	kfree_skb(skb);
 out_of_mem:
-	return 0;
+	return NET_RX_DROP;
 }
 
 /*
