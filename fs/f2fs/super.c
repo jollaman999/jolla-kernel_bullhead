2dd566edfe043284ef931654fe1bc843cd2269b2
jollaman999 <admin@jollaman999.com>
2016-04-01 20:11:40 +0900

    f2fs: Fix build error

--- fs/f2fs/super.c
+++ fs/f2fs/super.c
@@ -1272,12 +1272,6 @@ static int __f2fs_commit_super(struct f2fs_sb_info *sbi, int block)
 	struct buffer_head *bh;
 	int err;
 
-	if ((recover && f2fs_readonly(sbi->sb)) ||
-				bdev_read_only(sbi->sb->s_bdev)) {
-		set_sbi_flag(sbi, SBI_NEED_SB_WRITE);
-		return -EROFS;
-	}
-
 	bh = sb_getblk(sbi->sb, block);
 	if (!bh)
 		return -EIO;
@@ -1298,6 +1292,12 @@ static int __f2fs_commit_super(struct f2fs_sb_info *sbi, int block)
 int f2fs_commit_super(struct f2fs_sb_info *sbi, bool recover)
 {
 	int err;
+
+	if ((recover && f2fs_readonly(sbi->sb)) ||
+				bdev_read_only(sbi->sb->s_bdev)) {
+		set_sbi_flag(sbi, SBI_NEED_SB_WRITE);
+		return -EROFS;
+	}
 
 	/* write back-up superblock first */
 	err = __f2fs_commit_super(sbi, sbi->valid_super_block ? 0 : 1);
