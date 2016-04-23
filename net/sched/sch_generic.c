8961a6a85d71356467f31ab22d3b660e13e4d214
jollaman999 <admin@jollaman999.com>
2016-04-23 17:58:07 +0900

    Revert "net: sched: Change HZ to 1000 of watchdog time"
    This reverts commit 4ee52743a8e6bad2c21afb5a9fe0e9b4bd5cf822.

--- net/sched/sch_generic.c
+++ net/sched/sch_generic.c
@@ -270,7 +270,7 @@ void __netdev_watchdog_up(struct net_device *dev)
 {
 	if (dev->netdev_ops->ndo_tx_timeout) {
 		if (dev->watchdog_timeo <= 0)
-			dev->watchdog_timeo = 5*1000;
+			dev->watchdog_timeo = 5*HZ;
 		if (!mod_timer(&dev->watchdog_timer,
 			       round_jiffies(jiffies + dev->watchdog_timeo)))
 			dev_hold(dev);
