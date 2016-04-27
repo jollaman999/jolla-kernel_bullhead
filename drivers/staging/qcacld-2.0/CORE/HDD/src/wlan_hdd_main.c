eb83b13396c7ec4a80770efa79d32bd24a45e2c8
Subhani Shaik <subhanis@codeaurora.org>
2016-04-26 22:29:12 -0700

    qcacld-2.0: Reset wifi flag when wifi is turned off with static driver
    prima to qcacld-2.0 propagation
    
    In case of static driver, upon wifi turn off module exit
    doesn't happens. Module exit helps in cleanup of static memory.
    If driver is loaded statically, at the time of driver unload,
    wiphy flags are not cleared because of static memory.
    Fix it by removing channel type as static.
    
    CRs-Fixed: 944530
    
    Change-Id: Ibbd69eaaaf2f436d7f5f02efe63821b941f88b2f
    Signed-off-by: Subhani Shaik <subhanis@codeaurora.org>

--- drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-2.0/CORE/HDD/src/wlan_hdd_main.c
@@ -12902,7 +12902,7 @@ free_hdd_ctx:
 
    wlan_hdd_deinit_tx_rx_histogram(pHddCtx);
    wiphy_unregister(wiphy) ;
-
+   wlan_hdd_cfg80211_deinit(wiphy);
    wiphy_free(wiphy) ;
    if (hdd_is_ssr_required())
    {
@@ -15141,6 +15141,7 @@ err_ipa_cleanup:
 
 err_wiphy_unregister:
    wiphy_unregister(wiphy);
+   wlan_hdd_cfg80211_deinit(wiphy);
 
 err_vosclose:
    status = vos_sched_close( pVosContext );
