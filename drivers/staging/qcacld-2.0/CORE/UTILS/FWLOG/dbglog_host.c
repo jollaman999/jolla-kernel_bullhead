42eb077019d551de4ba072d6f0a5dd2cd47876c5
Steve Kondik <steve@cyngn.com>
2016-06-07 03:41:15 -0700

    qcacld-2.0: Chillax the spammy logging
     * Driver is hardcoded to build with -DDEBUG which upgrades all
       pr_debugs to be visible regardless of loglevel. This is
       incredibly spammy and expensive.
     * Add a kconfig to allow doing this if you really want it, otherwise
       these messages can be easily enabled using the dynamic debug
       mechanism.
    
    Change-Id: I6c763ca5338bb480a15de6d18dea99e1c81e1dbc

--- drivers/staging/qcacld-2.0/CORE/UTILS/FWLOG/dbglog_host.c
+++ drivers/staging/qcacld-2.0/CORE/UTILS/FWLOG/dbglog_host.c
@@ -55,8 +55,6 @@
 #include <a_debug.h>
 #define FWLOG_DEBUG   ATH_DEBUG_MAKE_MODULE_MASK(0)
 
-#if defined(DEBUG)
-
 static bool appstarted = FALSE;
 static bool senddriverstatus = FALSE;
 static bool kd_nl_init = FALSE;
@@ -65,6 +63,7 @@ static int get_version = 0;
 static int gprint_limiter = 0;
 static bool tgt_assert_enable = 0;
 
+#ifdef DEBUG
 static ATH_DEBUG_MASK_DESCRIPTION g_fwlogDebugDescription[] = {
     {FWLOG_DEBUG,"fwlog"},
 };
