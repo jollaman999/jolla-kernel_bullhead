b2c02f57dc67b00ed56a6dd98bc13f16b3ca154b
jollaman999 <admin@jollaman999.com>
2016-06-10 10:33:18 +0900

    Revert "msm: mdss: trigger frame timeout when recovery is in progress"
    This reverts commit 4c723b57dad7ea959b6175572e181675f19d3656.

--- drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_cmd.c
@@ -399,7 +399,7 @@ static void mdss_mdp_cmd_intf_recovery(void *data, int event)
 {
 	struct mdss_mdp_cmd_ctx *ctx = data;
 	unsigned long flags;
-	bool reset_done = false, notify_frame_timeout = false;
+	bool reset_done = false;
 
 	if (!data) {
 		pr_err("%s: invalid ctx\n", __func__);
@@ -426,19 +426,14 @@ static void mdss_mdp_cmd_intf_recovery(void *data, int event)
 	}
 
 	spin_lock_irqsave(&ctx->koff_lock, flags);
-	if (reset_done && atomic_add_unless(&ctx->koff_cnt, -1, 0)) {
-		pr_debug("%s: intf_num=%d\n", __func__, ctx->ctl->intf_num);
+	if (reset_done && atomic_read(&ctx->koff_cnt)) {
+		pr_debug("%s: intf_num=%d\n", __func__,
+					ctx->ctl->intf_num);
+		atomic_dec(&ctx->koff_cnt);
 		mdss_mdp_irq_disable_nosync(MDSS_MDP_IRQ_PING_PONG_COMP,
-			ctx->pp_num);
-
-		if (mdss_mdp_cmd_do_notifier(ctx))
-			notify_frame_timeout = true;
+						ctx->pp_num);
 	}
 	spin_unlock_irqrestore(&ctx->koff_lock, flags);
-
-	if (notify_frame_timeout)
-		mdss_mdp_ctl_notify(ctx->ctl, MDP_NOTIFY_FRAME_TIMEOUT);
-
 }
 
 static void mdss_mdp_cmd_pingpong_done(void *arg)
@@ -725,10 +720,8 @@ static int mdss_mdp_cmd_wait4pingpong(struct mdss_mdp_ctl *ctl, void *arg)
 		}
 		ctx->pp_timeout_report_cnt++;
 		rc = -EPERM;
-		if (atomic_add_unless(&ctx->koff_cnt, -1, 0)
-			&& mdss_mdp_cmd_do_notifier(ctx))
-			mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_TIMEOUT);
-
+		mdss_mdp_ctl_notify(ctl, MDP_NOTIFY_FRAME_TIMEOUT);
+		atomic_add_unless(&ctx->koff_cnt, -1, 0);
 	} else {
 		rc = 0;
 		ctx->pp_timeout_report_cnt = 0;
