1ffc1ee15c9ac9e8c9d81cd351866bd5b02fcc8a
franciscofranco <franciscofranco.1990@gmail.com>
2016-06-11 02:17:40 +0000

    Revert "timer: Queue timers on least power cpu"
    This reverts commit 9d2bc636283b62314eab8163545b48828a830bd8.

--- kernel/timer.c
+++ kernel/timer.c
@@ -765,8 +765,8 @@ __mod_timer(struct timer_list *timer, unsigned long expires,
 #endif
 
 #if defined(CONFIG_NO_HZ_COMMON) && defined(CONFIG_SMP)
-	if (!pinned && get_sysctl_timer_migration())
-		cpu = get_nohz_timer_target();
+		if (!pinned && get_sysctl_timer_migration() && idle_cpu(cpu))
+			cpu = get_nohz_timer_target();
 #endif
 		new_base = per_cpu(tvec_bases, cpu);
 
