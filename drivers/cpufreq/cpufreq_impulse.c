87080590641ae9d56ab0ef6b243a92c64a9bdaf0
Junjie Wu <junjiew@codeaurora.org>
2016-06-28 00:39:58 +0900

    cpufreq: impulse: Use wake_up_process_no_notif to wake up tasks
    Scheduler could send a notification to governor each time a task wakes
    up. If governor wakes up another task as a response to such a
    notification, it could result in endless recursive notifications.
    
    Use wake_up_process_no_notif to ensure scheduler won't send another
    notification for speedchange task woken up by the governor.
    
    Change-Id: I697affcbdf79e2ad0cfe843eb880d304960682f4
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>

--- drivers/cpufreq/cpufreq_impulse.c
+++ drivers/cpufreq/cpufreq_impulse.c
@@ -561,7 +561,7 @@ static void cpufreq_impulse_timer(unsigned long data)
 	spin_lock_irqsave(&speedchange_cpumask_lock, flags);
 	cpumask_set_cpu(max_cpu, &speedchange_cpumask);
 	spin_unlock_irqrestore(&speedchange_cpumask_lock, flags);
-	wake_up_process(speedchange_task);
+	wake_up_process_no_notif(speedchange_task);
 
 rearm:
 	if (!timer_pending(&ppol->policy_timer))
@@ -1609,7 +1609,7 @@ static int __init cpufreq_impulse_init(void)
 	get_task_struct(speedchange_task);
 
 	/* NB: wake up so the thread does not look hung to the freezer */
-	wake_up_process(speedchange_task);
+	wake_up_process_no_notif(speedchange_task);
 
 	return cpufreq_register_governor(&cpufreq_gov_impulse);
 }
