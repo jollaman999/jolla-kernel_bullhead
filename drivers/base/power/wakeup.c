51dc9041c91021247862758f119f0961bcb34595
Francisco Franco <franciscofranco.1990@gmail.com>
2016-08-31 17:43:03 +0000

    drivers: wakeup: remove bluesleep setting, it breaks everything if disabled
    Legend says bluesleep is a portal to the world of Argus. Unforunately
    closing it properly requires more power than a simple knob in the wakeup
    driver. May Guldan never hear of this...
    
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -26,8 +26,6 @@ static bool enable_wlan_ws = true;
 module_param(enable_wlan_ws, bool, 0644);
 static bool enable_bluedroid_timer_ws = true;
 module_param(enable_bluedroid_timer_ws, bool, 0644);
-static bool enable_bluesleep_ws = true;
-module_param(enable_bluesleep_ws, bool, 0644);
 
 /*
  * If set, the suspend/hibernate code will abort transitions to a sleep state
@@ -492,8 +490,7 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 	if (((!enable_bluedroid_timer_ws &&
 			!strcmp(ws->name, "bluedroid_timer")) ||
 		(!enable_wlan_wow_wl_ws && !strcmp(ws->name, "wlan_wow_wl")) ||
-		(!enable_wlan_ws && !strcmp(ws->name, "wlan")) ||
-		(!enable_bluesleep_ws && !strcmp(ws->name, "bluesleep")))) {
+		(!enable_wlan_ws && !strcmp(ws->name, "wlan")))) {
 		/*
 		 * let's try and deactivate this wakeup source since the user
 		 * clearly doesn't want it. The user is responsible for any
