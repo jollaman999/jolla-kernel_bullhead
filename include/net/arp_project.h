482ab0d220a352ef812f2c2b134e45275ceca86b
jollaman999 <admin@jollaman999.com>
2016-09-01 23:47:51 +0900

    arp, net/core: Fix exports
    Fix redefined compile errors.

--- include/net/arp_project.h
+++ include/net/arp_project.h
@@ -5,11 +5,4 @@
 #define ARP_PROJECT "arp_project: "
 #define ARP_PROJECT_VERSION "0.1"
 
-bool arp_project_enable;
-bool print_arp_info;
-bool ignore_gw_update_by_request;
-
-void arp_print_info(struct net_device *dev, struct arphdr *arp, int count);
-void arp_print_and_check_send(struct net_device *dev, struct sk_buff *skb);
-
 #endif	/* _ARP_PROJECT_H */
