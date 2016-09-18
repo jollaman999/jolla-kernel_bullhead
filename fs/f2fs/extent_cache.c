2df99d1140b874aef918150c1acda2bf24f93303
jollaman999 <admin@jollaman999.com>
2016-09-18 17:46:16 +0900

    Revert "f2fs: remove syncing inode page in all the cases"
    This reverts commit 098b6e7f7376feedd63e2ee9f82cd9ddf35e5e11.

--- fs/f2fs/extent_cache.c
+++ fs/f2fs/extent_cache.c
@@ -689,7 +689,9 @@ void f2fs_update_extent_cache(struct dnode_of_data *dn)
 
 	fofs = start_bidx_of_node(ofs_of_node(dn->node_page), dn->inode) +
 								dn->ofs_in_node;
-	f2fs_update_extent_tree_range(dn->inode, fofs, blkaddr, 1);
+
+	if (f2fs_update_extent_tree_range(dn->inode, fofs, blkaddr, 1))
+		sync_inode_page(dn);
 }
 
 void f2fs_update_extent_cache_range(struct dnode_of_data *dn,
@@ -699,7 +701,8 @@ void f2fs_update_extent_cache_range(struct dnode_of_data *dn,
 	if (!f2fs_may_extent_tree(dn->inode))
 		return;
 
-	f2fs_update_extent_tree_range(dn->inode, fofs, blkaddr, len);
+	if (f2fs_update_extent_tree_range(dn->inode, fofs, blkaddr, len))
+		sync_inode_page(dn);
 }
 
 void init_extent_cache_info(struct f2fs_sb_info *sbi)
