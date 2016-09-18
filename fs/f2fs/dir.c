90c36bdc151ceb4f5ec821d1050de05bd3508013
jollaman999 <admin@jollaman999.com>
2016-09-18 17:44:39 +0900

    Revert "f2fs: find parent dentry correctly"
    This reverts commit 5fe33d747cfa3344b78e3293a196793d9c7dc54b.

--- fs/f2fs/dir.c
+++ fs/f2fs/dir.c
@@ -268,9 +268,22 @@ out:
 
 struct f2fs_dir_entry *f2fs_parent_dir(struct inode *dir, struct page **p)
 {
-	struct qstr dotdot = QSTR_INIT("..", 2);
+	struct page *page;
+	struct f2fs_dir_entry *de;
+	struct f2fs_dentry_block *dentry_blk;
 
-	return f2fs_find_entry(dir, &dotdot, p);
+	if (f2fs_has_inline_dentry(dir))
+		return f2fs_parent_inline_dir(dir, p);
+
+	page = get_lock_data_page(dir, 0, false);
+	if (IS_ERR(page))
+		return NULL;
+
+	dentry_blk = kmap(page);
+	de = &dentry_blk->dentry[1];
+	*p = page;
+	unlock_page(page);
+	return de;
 }
 
 ino_t f2fs_inode_by_name(struct inode *dir, struct qstr *qstr)
