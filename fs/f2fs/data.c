6c7b076bf3ef8c43bb62f8b5d8de3c61bd3ee5c5
jollaman999 <admin@jollaman999.com>
2016-09-18 17:41:02 +0900

    Revert "f2fs: Remove wrong err check"
    This reverts commit 5aca23dcc4049501804300d77647b0b6e75e7dac.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1764,7 +1764,7 @@ static ssize_t f2fs_direct_IO(int rw, struct kiocb *iocb,
 							get_data_block_dio);
 	up_read(&F2FS_I(inode)->dio_rwsem[rw]);
 
-	if (rw & WRITE) {
+	if (err < 0 && (rw & WRITE)) {
 		if (err > 0)
 			set_inode_flag(inode, FI_UPDATE_WRITE);
 		else if (err < 0)
