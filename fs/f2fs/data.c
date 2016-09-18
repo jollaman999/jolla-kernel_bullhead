5e1edf4c87e0cb0da24de1423ff5a9a4436465bc
jollaman999 <admin@jollaman999.com>
2016-09-18 17:41:07 +0900

    Revert "f2fs: fix ERR_PTR returned by bio"
    This reverts commit 1532f18165e128a6ba453bd9bdb79d50b282b7b4.

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -1085,10 +1085,8 @@ submit_and_realloc:
 		}
 		if (bio == NULL) {
 			bio = f2fs_grab_bio(inode, block_nr, nr_pages);
-			if (IS_ERR(bio)) {
-				bio = NULL;
+			if (IS_ERR(bio))
 				goto set_error_page;
-			}
 		}
 
 		if (bio_add_page(bio, page, blocksize, 0) < blocksize)
