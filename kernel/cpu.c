3893478b88c0c40392407408d7131794010f2390
jollaman999 <admin@jollaman999.com>
2016-09-22 23:18:52 +0900

    Revert "msm_thermal, cpu: Skip cpu_up if offlined by core_control"
    This reverts commit 8ff1b840039c42a3d6f98f8440167035c921899f.

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -24,10 +24,6 @@
 
 #include "smpboot.h"
 
-#ifdef CONFIG_THERMAL_MONITOR
-#include <linux/msm_thermal.h>
-#endif
-
 #ifdef CONFIG_SMP
 /* Serializes the updates to cpu_online_mask, cpu_present_mask */
 static DEFINE_MUTEX(cpu_add_remove_lock);
@@ -451,11 +447,6 @@ int cpu_up(unsigned int cpu)
 #ifdef	CONFIG_MEMORY_HOTPLUG
 	int nid;
 	pg_data_t	*pgdat;
-#endif
-
-#ifdef CONFIG_THERMAL_MONITOR
-	if (cpus_offlined & BIT(cpu))
-		return 0;
 #endif
 
 	if (!cpu_possible(cpu)) {
