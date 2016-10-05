af7508a87169664b8ba7343aa67397fcfe6e00d7
jollaman999 <admin@jollaman999.com>
2016-10-05 21:46:42 +0900

    jolla-kernel_bullhead_II-v8.1

--- fs/proc/jolla-kernel.c
+++ fs/proc/jolla-kernel.c
@@ -7,7 +7,7 @@
 static int jolla_kernel_proc_show(struct seq_file *m, void *v)
 {
 	seq_printf(m, "{\"kernel-name\": \"jolla-kernel_bullhead_II\","
-			"\"version\": \"v8.0\","
+			"\"version\": \"v8.1\","
 			"\"buildtime\": \"%s\"}\n", JOLLA_KERNEL_TIMESTAMP);
 	return 0;
 }
