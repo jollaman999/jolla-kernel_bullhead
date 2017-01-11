1307ea20ffa638cf78dc50369a093e4c7084f271
jollaman999 <admin@jollaman999.com>
2017-01-11 23:52:16 +0900

    Revert "sdcardfs: Remove stale dentries when reusing an inode."
    This reverts commit 889fd93d2f1dd42ca9d3ad0ac065a84d8a180f6e.

--- fs/sdcardfs/lookup.c
+++ fs/sdcardfs/lookup.c
@@ -111,13 +111,8 @@ struct inode *sdcardfs_iget(struct super_block *sb, struct inode *lower_inode, u
 		return ERR_PTR(err);
 	}
 	/* if found a cached inode, then just return it */
-	if (!(inode->i_state & I_NEW)) {
-		/* There can only be one alias, as we don't permit hard links
-		 * This ensures we do not keep stale dentries that would later
-		 * cause confusion. */
-		d_prune_aliases(inode);
+	if (!(inode->i_state & I_NEW))
 		return inode;
-	}
 
 	/* initialize new inode */
 	info = SDCARDFS_I(inode);
