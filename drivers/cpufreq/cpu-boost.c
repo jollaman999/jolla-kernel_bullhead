acda2a4e0f31d1bd55b539feee08087f49509d5d
jollaman999 <admin@jollaman999.com>
2017-02-22 22:39:57 +0900

    cpufreq: cpu-boost: Disable input boost by default

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -51,7 +51,7 @@ module_param(boost_ms, uint, 0644);
 static unsigned int sync_threshold;
 module_param(sync_threshold, uint, 0644);
 
-static unsigned int input_boost_enabled = 1;
+static unsigned int input_boost_enabled = 0;
 module_param(input_boost_enabled, uint, 0644);
 
 static unsigned int input_boost_ms = 1500;
