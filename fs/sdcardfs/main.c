85843e2aecf2377c6208a027269f9dfa710d9bb5
jollaman999 <admin@jollaman999.com>
2018-11-29 00:01:20 +0900

    Revert "ANDROID: sdcardfs: Add default_normal option"
    This reverts commit 522f89a229db3e8729604125dc41db4e976c33b4.

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -33,7 +33,6 @@ enum {
 	Opt_userid,
 	Opt_reserved_mb,
 	Opt_gid_derivation,
-	Opt_default_normal,
 	Opt_err,
 };
 
@@ -46,7 +45,6 @@ static const match_table_t sdcardfs_tokens = {
 	{Opt_userid, "userid=%d"},
 	{Opt_multiuser, "multiuser"},
 	{Opt_gid_derivation, "derive_gid"},
-	{Opt_default_normal, "default_normal"},
 	{Opt_reserved_mb, "reserved_mb=%u"},
 	{Opt_err, NULL}
 };
@@ -70,7 +68,6 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	opts->reserved_mb = 0;
 	/* by default, gid derivation is off */
 	opts->gid_derivation = false;
-	vfsopts->default_normal = false;
 
 	*debug = 0;
 
@@ -125,8 +122,6 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 		case Opt_gid_derivation:
 			opts->gid_derivation = true;
 			break;
-		case Opt_default_normal:
-			vfsopts->default_normal = true;
 		/* unknown option */
 		default:
 			if (!silent)
@@ -180,7 +175,6 @@ int parse_options_remount(struct super_block *sb, char *options, int silent,
 				return 0;
 			vfsopts->mask = option;
 			break;
-		case Opt_default_normal:
 		case Opt_multiuser:
 		case Opt_userid:
 		case Opt_fsuid:
