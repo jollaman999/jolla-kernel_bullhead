c389d7ca8a202a5748946a2bc4d032fcfe58b401
jollaman999 <admin@jollaman999.com>
2018-11-29 00:01:19 +0900

    Revert "ANDROID: sdcardfs: Fix missing break on default_normal"
    This reverts commit b1ec360042ed08afcc35a016ffbe83773784e769.

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -127,7 +127,6 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 			break;
 		case Opt_default_normal:
 			vfsopts->default_normal = true;
-			break;
 		/* unknown option */
 		default:
 			if (!silent)
